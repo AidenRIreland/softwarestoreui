eb301c0c3e477ed4fb7d1e7a8ee551924e8655e4
AidenRIreland <aidenireland2004@gmail.com>
2023-11-22 02:46:35 -0500

    Finish nav

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Home from './Home';
 import { Login } from "./Login";
 import { Register } from "./Register";
+import logo from './mysoftwarestorewebp.png'
+import cart from './Cart.png'
 
 function App() {
   const [currentForm, setCurrentForm] =useState('login');
@@ -12,24 +14,29 @@ function App() {
   }
   return (
     <Router>
-        <div className="App">
-          <head>
-          <meta charset="utf-8"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1"/>
-          <title>MySoftwareStore</title>
-          </head>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
-                <Link to=""></Link>
-            </nav>
-            <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />} />
-                </Routes>
+      <div className="App">
+        <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>MySoftwareStore</title>
+        </head>
+        <nav>
+        <div className="nav-left">
+          <img src={logo} alt="Logo" className="nav-logo" />
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
         </div>
-    </Router>
-);
-}
-
+        <div className="nav-right">
+          <input type="text" placeholder="Search..." className="search-bar"/>
+          <Link to="/cart"><img src={cart} alt="Cart" className="cart-logo" /></Link>
+        </div>
+        </nav>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />} />
+          </Routes>
+        </div>
+      </Router>
+   );
+  }
 export default App;
